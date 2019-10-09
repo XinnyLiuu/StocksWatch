9811b9e6d41b4f28b5cb5e0a99a4de420d4563ca
Xin Liu <xhliu97@gmail.com>
2019-10-09 13:47:58 -0400

    added logging and printing of routes

--- server/app.js
+++ server/app.js
@@ -1,15 +1,20 @@
 'use strict';
 const express = require('express');
+const morgan = require('morgan');
 
 // Create express server
 const app = express();
+
+// Setup logger
+app.use(morgan(':date[iso] :status :method :url [:response-time ms] :remote-addr'));
+
+// Start server
+app.listen(8000);
+console.log("Server running on port 8000");
 
 // Services
 const monthlyDataService = require('./services/monthly.js');
 
 // Routes
 app.get("/api/monthly/:symbol", monthlyDataService.getStockDataBySymbol);
-
-// Start server
-app.listen(8000);
-console.log("Server running on port 8000");
+console.log(app._router.stack[3]["route"]["path"]);
