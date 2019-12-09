f7c5e57e0ec73dc4586c59f79aa5023af89039cf
BenThorn <brt4557@g.rit.edu>
2019-12-08 23:41:10 -0500

    added electron support

---
+++ electron-starter.js
@@ -0,0 +1,36 @@
+const {app, BrowserWindow, Menu} = require('electron')
+
+// Remove top toolbar
+Menu.setApplicationMenu(false)
+
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({width: 1200, height: 800});
+
+    mainWindow.setMenuBarVisibility(false);
+
+    mainWindow.loadURL('http://stockswatch.tk/');
+
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+app.on('ready', createWindow);
+
+// So that the app stops on window close only on OSes that support it
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow()
+        mainWindow.removeMenu();
+    }
+});
